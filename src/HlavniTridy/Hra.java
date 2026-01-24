d995a3c870b3ee0e2e009ef443de3d8b68f5d200
Halas <halas300bmx@gmail.com>
2026-01-24 19:31:31 +0100

    Update 5:
    První řádky ve třídě hra (Zprovoznění příkazů):
    1. Start - Načte data a příkazy, Nastaví hráči úvodní lokaci a spustí while pro chod hry
    2. ZacniPrikazy - Přidání všech příkazů do hry
    3. ZpracujPrikaz - Metoda pro zpracovávání příkazů od hráče tak aby mohl program vyhledávat příkaz v seznamPrikazu a zavolat metodu proved
    
    TODO1: Začít psát příkazy jde, vezmi, mluv atd.
    TODO2: Začít psát komentáře
    TOD3: Začít vytvářet dějovou linku hry

--- src/HlavniTridy/Hra.java
+++ src/HlavniTridy/Hra.java
@@ -1,4 +1,62 @@
 package HlavniTridy;
 
+import PridaneVeci.Mistnosti;
+import Prikazy.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Hra {
+    public static Mistnosti aktualniLokace;
+    public static GameData data;
+    private ArrayList<Prikaz> seznamPrikazu = new ArrayList<>();
+    boolean konec = false;
+
+    public void start() {
+        try {
+            data = GameData.nactiData("/gamedata.json");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        aktualniLokace = data.najdiMistnost("loc_motorhome");
+        if (aktualniLokace == null) {
+            System.out.println("Chyba");
+            return;
+        }
+        zacniPrikazy();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" F1 - Velké Finále ");
+        System.out.println("Jsi v: " + aktualniLokace.getNazev());
+        while (!konec) {
+            String vstup = sc.nextLine();
+            zpracujPrikaz(vstup);
+        }
+    }
+
+    private void zacniPrikazy() {
+        seznamPrikazu.add(new PrikazJdi());
+        seznamPrikazu.add(new PrikazNapoveda());
+        seznamPrikazu.add(new PrikazBatoh());
+        seznamPrikazu.add(new PrikazVezmi());
+        seznamPrikazu.add(new PrikazMapa());
+        seznamPrikazu.add(new PrikazMluv());
+        seznamPrikazu.add(new PrikazNastav());
+        seznamPrikazu.add(new PrikazPouzij());
+    }
+
+    private void zpracujPrikaz(String radek) {
+        String[] slova = radek.trim().split(" ");
+        String nazev = slova[0].toLowerCase();
+        if (nazev.equals("konec")) {
+            konec = true;
+            return;
+        }
+        for (Prikaz p : seznamPrikazu) {
+            if (p.getNazev().equals(nazev)) {
+                p.proved(slova);
+                return;
+            }
+        }
+        System.out.println("Neznámý příkaz.");
+    }
 }
