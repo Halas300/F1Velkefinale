18f6ba1325dc53a6a26683d96ddbe39617a77038
Halas <halas300bmx@gmail.com>
2026-01-24 17:47:03 +0100

    Update 4:
    Vytvoření třídy GameData: Vytvoření načtení dat a hledání místností a věcí
    
    TODO1: Zprovoznit příkaz Jdi
    TODO2: Začít psát třídu hra a začít zprovoznovat příkazy
    TODO3: Začít psát komentáře

---
+++ src/HlavniTridy/GameData.java
@@ -0,0 +1,44 @@
+package HlavniTridy;
+
+import PridaneVeci.Veci;
+import PridaneVeci.Mistnosti;
+import PridaneVeci.NPC;
+import com.google.gson.Gson;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class GameData {
+    public ArrayList<Veci> veci;
+    public ArrayList<NPC> npc;
+    public ArrayList<Mistnosti> mistnosti;
+
+    public static GameData nactiData (String resourcePath) {
+        Gson gson = new Gson();
+        try (InputStream is = GameData.class.getResourceAsStream(resourcePath)) {
+            if (is == null) {
+                throw new IllegalStateException("Soubor " + resourcePath + " nebyl nalezen");
+            }
+            return gson.fromJson(new InputStreamReader(is, StandardCharsets.UTF_8), GameData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
+        }
+    }
+
+    public Mistnosti najdiMistnost(String id) {
+        for (Mistnosti l : mistnosti) {
+            if (l.getId().equals(id)) {
+                return l;
+            }
+        }
+        return null;
+    }
+
+    public Veci NajdiVec(String id) {
+        for (Veci i : veci) {
+            if (i.getId().equals(id)) return i;
+        }
+        return null;
+    }
+}
