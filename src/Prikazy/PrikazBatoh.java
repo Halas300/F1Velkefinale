bad15ca748c9db4861beeaed81c4f5f41be1ad90
Halas <halas300bmx@gmail.com>
2026-01-30 00:44:17 +0100

    Update 8:
    Vytvoření třídy batoh:
    Nejdříve se program podívá zda je batoh prázdný nebo ne. Pokud ano tak to napíše že je batoh prázdný. Pokud ne, tak program vypíše věci co jsou v batohu a místo v batohu
    
    TODO1: Začít psát ostatní příkazy
    TODO2: Začít psát komentáře
    TODO3: Začít vytvářet dějovou linku hry

--- src/Prikazy/PrikazBatoh.java
+++ src/Prikazy/PrikazBatoh.java
@@ -1,5 +1,8 @@
 package Prikazy;
 
+import HlavniTridy.StavHrace;
+import PridaneVeci.Veci;
+
 public class PrikazBatoh implements Prikaz {
 
     @Override
@@ -9,6 +12,15 @@ public class PrikazBatoh implements Prikaz {
 
     @Override
     public void proved(String[] parametry) {
+        if (StavHrace.batoh.isEmpty()) {
+            System.out.println("Batoh je prázdný.");
+            return;
+        }
 
+        for (Veci v : StavHrace.batoh) {
+            System.out.println(v.getNazev() + " (" + v.getPopis() + ")");
+        }
+
+        System.out.println("Místo: " + StavHrace.batoh.size() + "/" + StavHrace.KapacitaBatohu);
     }
 }
