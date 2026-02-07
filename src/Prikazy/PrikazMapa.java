fa71deb9bc856555905b9ad00d04cf710e9a9631
Halas <halas300bmx@gmail.com>
2026-02-07 20:06:19 +0100

    Update 25:
    Upravení třídy PříkazMapa
    
    TODO1: Začít psát Příkazy, které je potřeba dodělat
    TODO2: Začít psát komentáře
    TODO3: Začít vytvářet dějovou linku hry + dialogy v příkazu mluv
    TODO4: Přidat další items

--- src/Prikazy/PrikazMapa.java
+++ src/Prikazy/PrikazMapa.java
@@ -1,4 +1,5 @@
 package Prikazy;
+import HlavniTridy.TextoveData;
 
 public class PrikazMapa implements Prikaz {
 
@@ -8,38 +9,8 @@ public class PrikazMapa implements Prikaz {
     }
 
     @Override
-    public void proved(String[] parametry) {
-        System.out.println("                         ┌───────────────┐           ");
-        System.out.println("                         │ 2. VIP LOUNGE │           ");
-        System.out.println("                         │  (Christian)  │           ");
-        System.out.println("                         └───────┬───────┘           ");
-        System.out.println("                                 │                   ");
-        System.out.println("              ┌───────────────┐  │  ┌───────────────┐");
-        System.out.println("              │ 1. MOTORHOME  │  │  │ 3. MEDIA CTR  │");
-        System.out.println("              │   Odpočinek   │  │  │   Rozhovory   │");
-        System.out.println("              └───────┬───────┘  │  └───────┬───────┘");
-        System.out.println("                      │          │          │        ");
-        System.out.println("                      │   ┌──────▼──────────▼─────┐  ");
-        System.out.println("                      └──▶│        PADDOCK        │  ");
-        System.out.println("                          │    Centrální zóna     │  ");
-        System.out.println("                     ┌───▶│  Pohyb mezi lokacemi  │  ");
-        System.out.println("                     │    └────────────────▲──────┘  ");
-        System.out.println("                     │                     │         ");
-        System.out.println("             ┌───────────────┐    ┌───────────────┐  ");
-        System.out.println("             │ 4. TECH CENTR │    │   5. GARÁŽ    │  ");
-        System.out.println("             │   Data / AI   │ ┌─▶│  Úpravy auta  │  ");
-        System.out.println("             └───────────────┘ │  └───────▲───────┘  ");
-        System.out.println("                               │          │          ");
-        System.out.println("                               │   ┌──────────────┐  ");
-        System.out.println("                               │   │   6. SKLAD   │  ");
-        System.out.println("                               │   │  Součástky   │  ");
-        System.out.println("                               │   └──────────────┘  ");
-        System.out.println("                               │                     ");
-        System.out.println("                        ┌──────────────┐             ");
-        System.out.println("                        │  7. PIT LANE │             ");
-        System.out.println("                        │  Závod/Pneu  │             ");
-        System.out.println("                        └──────────────┘             ");
-
+    public String proved(String[] parametry) {
+        return TextoveData.nactiText("/napoveda.txt");
     }
 }
 
