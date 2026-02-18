abae30aaa7d300c8331e6df2f28871eb79bdd570
Halas <halas300bmx@gmail.com>
2026-02-18 18:00:23 +0100

    Update 43:
    Přidání podmínek, které musí hráč splnoval pro to, aby mohl jít do kvalifikace
    
    TODO1: Dodělat kvalifikaci
    TODO2: Udělat Závod
    TODO3: Začít psát komentáře

--- src/Prikazy/PrikazJdi.java
+++ src/Prikazy/PrikazJdi.java
@@ -1,6 +1,7 @@
 package Prikazy;
 
 import HlavniTridy.Hra;
+import HlavniTridy.StavHrace;
 import PridaneVeci.Mistnosti;
 
 public class PrikazJdi implements Prikaz {
@@ -20,6 +21,26 @@ public class PrikazJdi implements Prikaz {
         for (String idSouseda : Hra.aktualniLokace.soused) {
             Mistnosti sousedLokace = Hra.data.najdiMistnost(idSouseda);
             if (sousedLokace.getNazev().toLowerCase().contains(cil.toLowerCase())) {
+                if (sousedLokace.getId().equals("loc_pitlane")) {
+
+                    boolean vseSplneno = true;
+                    String chybi = "";
+                    if (!StavHrace.autoOpraveno) {
+                        vseSplneno = false;
+                        chybi = chybi + "Opravit auto\n";
+                    }
+                    if (!StavHrace.dataStazena) {
+                        vseSplneno = false;
+                        chybi = chybi + "Stáhnout data \n";
+                    }
+                    if (StavHrace.nastavenoKridlo == 0) {
+                        vseSplneno = false;
+                        chybi = chybi + "Nastavit křídlo \n";
+                    }
+                    if (!StavHrace.rozhovorDokoncen) {
+                        vseSplneno = false;
+                        chybi = chybi + "Udělat rozhovor se Sarah\n";
+                    }
                 Hra.aktualniLokace = sousedLokace;
                 return "Přešel jsi do: " + sousedLokace.getNazev() + "\n" +
                         sousedLokace.getPopis();
